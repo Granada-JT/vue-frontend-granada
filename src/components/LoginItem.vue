dd2f33b1beb93124154765bbbb7eecc6a0e63afe
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-10 02:07:48 +0800

    feat: add login page form

--- src/components/LoginItem.vue
+++ src/components/LoginItem.vue
@@ -1,37 +1,17 @@
 <template>
   <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
-    </div>
+    <h3>
+      <slot name="heading"></slot>
+    </h3>
+    <slot></slot>
   </div>
 </template>
 
 <style scoped>
-.item {
-  margin-top: 2rem;
-  display: flex;
-  position: relative;
-}
 
 .details {
   flex: 1;
   margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
 }
 
 h3 {
@@ -41,9 +21,13 @@ h3 {
   color: var(--color-heading);
 }
 
+.item {
+  margin-top: 30px;
+}
+
 @media (min-width: 1024px) {
   .item {
-    margin-top: 0;
+    margin-top: 0px;
     padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
   }
 
