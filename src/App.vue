406d9e128856ac0d8847e61fc7d1eb4a9ff053e4
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-10 02:07:48 +0800

    feat: add login page form

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <!-- <nav> -->
+        <!-- ToDo: Conditionally Render to Logout when user is logged in -->
+        <!-- <RouterLink to="/">Login</RouterLink> -->
+        <!-- ToDo: Enable home when user has successfully logged in -->
+        <!-- <RouterLink to="/about">Home</RouterLink> -->
+      <!-- </nav> -->
     </div>
   </header>
 
