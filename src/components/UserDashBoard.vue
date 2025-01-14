00e8424bb3b0d2591a88f5a5133fb6bc2096205e
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-15 01:12:40 +0800

    feat: add user and role dashboards

---
+++ src/components/UserDashBoard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { useUsersStore } from '@/stores/users'
+
+const authStore = useAuthStore()
+const usersStore = useUsersStore()
+</script>
+
+<template>
+		<div class="user-dashboard">User DashBoard</div>
+</template>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	.user-dashboard {
+		height: 500px;
+		width: 500px
+	}
+
+	.logout {
+		background-color: brown;
+	}
+
+	.logout:hover {
+		background-color: rgb(233, 43, 43);
+	}
+</style>
