bea255d78af2be6fdaec2ec357a7b4bd220fea8e
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-15 03:01:37 +0800

    feat: add create user form

--- src/views/DashBoardView.vue
+++ src/views/DashBoardView.vue
@@ -3,8 +3,6 @@ import RoleDashBoard from '../components/RoleDashBoard.vue'
 import { useAuthStore } from '@/stores/auth'
 
 const authStore = useAuthStore()
-
-console.log('dashboard isloggedin: ', authStore.isLoggedIn)
 </script>
 
 <template>
