b76678948911be379a37e3d3addb7575785f23a5
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-17 02:22:11 +0800

    feat: add create user api call

--- src/components/UserDashBoard.vue
+++ src/components/UserDashBoard.vue
@@ -1,24 +1,45 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useAuthStore } from '@/stores/auth'
-import { useUsersStore } from '@/stores/users'
-import type { CreateUser } from '@/types/users'
+import axios from 'axios'
 
-const authStore = useAuthStore()
-const usersStore = useUsersStore()
-
-const fullname = ref('')
-const email = ref('')
-const nominatedPassword = ref('')
-const confirmPassword = ref('')
-const role = ref('')
+const roleId = ref(0)
 
 const handleSubmit = async (e: Event) => {
   try {
     const formData = new FormData(e.target as HTMLFormElement);
     const values = Object.fromEntries(formData.entries());
+		if (values.nominatedPassword !== values.confirmPassword) {
+      throw new Error('Passwords do not match.');
+    }
+
+		const payload = {
+			name: values.fullname,
+			email: values.email,
+			password: values.confirmPassword,
+			role_id: roleId.value
+		}
+
+		await axios.get('http://localhost:8000/sanctum/csrf-cookie', { withCredentials: true });
+
+		const xsrfTokenRow = document.cookie
+			.split('; ')
+			.find((row) => row.startsWith('XSRF-TOKEN='));
+
+		const xsrfToken = xsrfTokenRow ? decodeURIComponent(xsrfTokenRow.split('=')[1]) : undefined;
+
+		const response = await axios.post('http://localhost:8000/users', payload,
+			{
+        headers: {
+          'X-XSRF-TOKEN': xsrfToken,
+        },
+        withCredentials: true,
+      }
+		)
+
+		if (response.status === 201) {
+			(e.target as HTMLFormElement).reset();
+		}
 		
-		// ToDo: Add API Call
   } catch (error) {
     console.error(error);
   }
@@ -33,23 +54,23 @@ const handleSubmit = async (e: Event) => {
 			<label>
 				Full Name:
 			</label>
-				<input type="text" name="fullname" v-model="fullname" />
+				<input type="text" name="fullname" />
 			<label>
 				Email Address:
 			</label>
-			<input type="email" name="email" v-model="email" />
+			<input type="email" name="email" />
 			<label>
 				Password:
 			</label>
-			<input type="password" name="nominatedPassword" v-model="nominatedPassword" />
+			<input type="password" name="nominatedPassword" />
 			<label>
 				Confirm Password:
 			</label>
-			<input type="password" name="confirmPassword" v-model="confirmPassword" />
+			<input type="password" name="confirmPassword" />
 			<label>
 				Assign Role:
 			</label>
-			<input type="text" name="role" v-model="role" />
+			<input type="number" name="role" v-model="roleId"/>
 			<button type="submit">Create User</button>
 		</form>
 	</div>
