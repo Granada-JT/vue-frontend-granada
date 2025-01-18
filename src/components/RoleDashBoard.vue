0e554b82100bbb4d7269db1ff199a43e0856114b
Jomar Tabios Granada <jomart.granada@gmail.com>
2025-01-19 00:05:58 +0800

    feat: add create role api call

--- src/components/RoleDashBoard.vue
+++ src/components/RoleDashBoard.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
 import { useRolesStore } from '@/stores/roles'
 
 const rolesStore = useRolesStore()
+const roleName = ref<string>('')
+const roleDescription = ref<string>('')
 
 const handleSubmit = async (e: Event) => {
   try {
-    const formData = new FormData(e.target as HTMLFormElement);
-    const values = Object.fromEntries(formData.entries());
-		
-		// ToDo: Add API Call
+		await axios.get('http://localhost:8000/sanctum/csrf-cookie', { withCredentials: true });
+
+		const xsrfTokenRow = document.cookie
+			.split('; ')
+			.find((row) => row.startsWith('XSRF-TOKEN='));
+
+		const xsrfToken = xsrfTokenRow ? decodeURIComponent(xsrfTokenRow.split('=')[1]) : undefined;
+
+		const payload = {
+			name: roleName.value,
+			description: roleDescription.value
+		}
+
+		const response = await axios.post('http://localhost:8000/roles', payload,
+			{
+				headers: {
+					'X-XSRF-TOKEN': xsrfToken,
+				},
+				withCredentials: true,
+			}
+		)
+
+		if (response.status === 201) {
+			// ToDo Replace with toast
+			console.log('Role Created Successfuly');
+			(e.target as HTMLFormElement).reset()
+		}
+
   } catch (error) {
     console.error(error);
   }
@@ -29,9 +57,9 @@ const handleUpdate = async (roleId: number) => {
 			<form @submit.prevent="handleSubmit">
 				<h1>Create Role</h1>
 				<label>Role Name:</label>
-				<input type="text" name="roleName" />
+				<input type="text" name="roleName" v-model="roleName"/>
 				<label>Role Description:</label>
-				<input type="text" name="roleDescription" />
+				<input type="text" name="roleDescription" v-model="roleDescription" />
 				<button type="submit">Create role</button>
 			</form>
 			<div class="table">
